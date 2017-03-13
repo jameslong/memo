bdab31870762b3bb4ce1e698d033e2d4220f3fb7
James Long <jamesharwoodlong@gmail.com>
2017-03-13 11:31:59 +0000

    Remove kbpgp dependencies
    Encryption is not used in this version of the tools.

--- browser/requireconfig.js
+++ browser/requireconfig.js
@@ -4,7 +4,6 @@ require.config({
         deps: ["./browser/src/main"],
         paths: {
                 "jquery": "../../node_modules/jquery/dist/jquery",
-                "kbpgp": "lib/kbpgp/kbpgp-2.0.8",
                 "react": "../../node_modules/react/dist/react",
                 "react-dom": "../../node_modules/react-dom/dist/react-dom"
         },
